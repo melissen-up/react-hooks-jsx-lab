617b1550ca1cba9aa404aa005867eb735ff928a2
Melissa Faulner <melissa.faulner@gmail.com>
2021-02-22 21:04:30 -0500

    commit tree 851fbfd42e41e724eb1f2595d2e4d7ce4120890a

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,13 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>I made it through the wilderness. Somehow I made it through. Didn't know how lost I was until I found you.</p>
+      <img src={image} alt="I made this"></img>
+    </div>
+  )
 }
 
 export default About;
